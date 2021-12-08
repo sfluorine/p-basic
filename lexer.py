efc4f3f4e0865d84685642b683f46b0d62fa770d
daffa_akram <diablodvorak@gmail.com>
2021-12-08 16:24:01 +0700

    adding support for identifier

--- lexer.py
+++ lexer.py
@@ -10,6 +10,9 @@ class TokenType(Enum):
     BINOP_DIV = 5
     LITERAL_NUMBER = 6
     LITERAL_STRING = 7
+    KEYWORD_DEFINE = 8
+    EQUAL = 9
+    IDENTIFIER = 10
 
 class Token:
     def __init__(self, tk, rep):
@@ -26,7 +29,7 @@ class Lexer:
 
     def error_and_die(self, message):
         print(f"File: \"<{self.file_name}>\"")
-        print(f"[ERROR]: {message} \n    at line: {self.current_line}")
+        print(f"[ERROR]: {message}, at Line: {self.current_line}")
         sys.exit(1)
 
     def advance(self):
@@ -67,6 +70,9 @@ class Lexer:
             token = Token(TokenType.BINOP_MUL, self.current_char)
         elif self.current_char == '/':
             token = Token(TokenType.BINOP_DIV, self.current_char)
+        elif self.current_char == '=':
+            token = Token(TokenType.EQUAL, self.current_char)
+        # Search for string
         elif self.current_char == '"':
             start_pos = self.current_index
             string = ''
@@ -82,12 +88,14 @@ class Lexer:
                 self.error_and_die(f"( {self.source[start_pos:end_pos:]} ) \nmissing double quotes after opening quotes")
 
             token = Token(TokenType.LITERAL_STRING, string)
+        # Search for number
         elif self.current_char >= '0' and self.current_char <= '9':
             decimal_count = 0
             num_str = ''
             num_str += self.current_char
             self.advance()
 
+            # Keep appending if current character is number
             while (self.current_char >= '0' and self.current_char <= '9') or self.current_char == '.':
                 if self.current_char == '.':
                     decimal_count += 1
@@ -101,6 +109,20 @@ class Lexer:
                 num_str += '0'
 
             token = Token(TokenType.LITERAL_NUMBER, num_str)
+        # Search for identifier or keyword
+        elif (self.current_char >= 'a' and self.current_char <= 'z') or self.current_char >= 'A' and self.current_char <= 'Z':
+            identifier_str = ''
+            identifier_str += self.current_char
+            self.advance()
+
+            while (self.current_char >= 'a' and self.current_char <= 'z') or self.current_char >= 'A' and self.current_char <= 'Z':
+                identifier_str += self.current_char
+                self.advance()
+
+            if identifier_str == 'define':
+                token = Token(TokenType.KEYWORD_DEFINE, identifier_str)
+            else:
+                token = Token(TokenType.IDENTIFIER, identifier_str)
         else:
             self.error_and_die(f"unexpected token \"{self.current_char}\"")
 
